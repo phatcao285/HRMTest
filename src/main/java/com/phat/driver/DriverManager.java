46c948b259414e8c40e5fe8f7935075395424905
phatcao285 <2124802010172@student.tdmu.edu.vn>
2025-10-02 16:38:15 +0700

    Create Driver

---
+++ src/main/java/com/phat/driver/DriverManager.java
@@ -0,0 +1,21 @@
+package com.phat.driver;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();// Execute in parrallel with distinct WebDriver instance per thread
+    public static WebDriver getDriver(){
+        return driver.get();// get value of driver in ThreadLocal
+    }
+    public static void setDriver(WebDriver driver){
+        DriverManager.driver.set(driver);
+        // assign Webdriver to ThreadLocal
+    }
+    public static void quit(){
+        DriverManager.driver.get().quit();// Close current driver session
+        driver.remove();// Remove Thread
+    }
+
+}
+// If run parallel with single driver in basetest will cause NoSuchSessionException
+// ThreadLocal store distinct driver thread initialized
